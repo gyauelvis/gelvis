11d6e8f8e7d722e4c5458d6135dcc197f3aa60f4
g.elvis <gyauelvis@gmail.com>
2023-09-04 15:35:35 +0000

    footer

---
+++ src/components/footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer>
+        <span>Made with 🖤 from Asankran-Breman</span>
+    </footer>
+</template>
+<script>
+export default{
+    name: 'Footer'
+}
+
+</script>
+<style scoped>
+
+footer{
+    width: 100%;
+    display:flex;
+    
+}
+
+</style>
